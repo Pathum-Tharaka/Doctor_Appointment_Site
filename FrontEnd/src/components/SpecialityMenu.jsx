ea29cc320db28045c1057c63add1853c0141af94
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-10-22 10:34:43 +0530

    specialitymenu update

--- FrontEnd/src/components/SpecialityMenu.jsx
+++ FrontEnd/src/components/SpecialityMenu.jsx
@@ -13,15 +13,19 @@ const SpecialityMenu = () => {
         Simply browse through our extensive list of trusted doctors, schedule
         your appointment hassle-free.
       </p>
-      <div className="flex sm:justify-center gap-4 pt-5 w-full overflow-scroll">
+      <div className="flex sm:justify-center gap-4 pt-5 w-full overflow-x-auto">
         {specialityData.map((item, index) => (
           <Link
-          onClick={() => window.scrollTo(0, 0)}
+            onClick={() => window.scrollTo(0, 0)}
             className="flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500"
             key={index}
-            to={"/doctors/${item.speciality}"}
+            to={`/doctors/${item.speciality}`}  
           >
-            <img className="w-16 sm:w-24 mb-2" src={item.image} alt="" />
+            <img 
+              className="w-16 sm:w-24 mb-2" 
+              src={item.image} 
+              alt={item.speciality} 
+            />
             <p>{item.speciality}</p>
           </Link>
         ))}
