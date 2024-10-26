8cf1e21b50758d81fe5a24d4bec329a333173e35
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-10-27 00:57:35 +0530

    summery

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,13 +2,12 @@ import express from "express";
 import 'dotenv/config';
 import cors from "cors";
 
-import connectDB from "./config/mongodb.js";
+// import connectDB from "./config/mongodb.js";
 
 //app config
 const app = express();
 const port = process.env.PORT || 4000;
 
-connectDB()
 
 //middleware
 app.use(express.json());
@@ -22,18 +21,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-// //routes
-// import authRoutes from './routes/auth.js';
-// import userRoutes from './routes/users.js';
-// import doctorRoutes from './routes/doctor.js';
-// import bookingRoutes from './routes/booking.js';
-// import messageRoutes from './routes/message.js';
-
-// app.use('/api/v1/auth', authRoutes);
-// app.use('/api/v1/users', userRoutes);
-// app.use('/api/v1/doctors', doctorRoutes);
-// app.use('/api/v1/bookings', bookingRoutes);
-// app.use('/api/v1/messages', messageRoutes);
-
-// app.get('/', (req, res) => {
