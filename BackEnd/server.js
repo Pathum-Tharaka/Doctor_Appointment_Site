920bcb6104559a57f157338b8fbc4ec198eb9212
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-10-28 01:00:36 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,13 +1,15 @@
 import express from "express";
-import 'dotenv/config';
 import cors from "cors";
+import 'dotenv/config';
+import connectDB from "./config/mongodb.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-// import connectDB from "./config/mongodb.js";
 
 //app config
-const app = express();
-const port = process.env.PORT || 4000;
-
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
 
 //middleware
 app.use(express.json());
