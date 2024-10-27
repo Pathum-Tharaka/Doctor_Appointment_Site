a8146cd8474b9784d15fce4f3c87c5ad8ff162ee
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-10-28 01:03:00 +0530

    Update mongodb.js

--- BackEnd/config/mongodb.js
+++ BackEnd/config/mongodb.js
@@ -1,28 +1,11 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config(); // Load environment variables
 
 const connectDB = async () => {
-    try {
-        mongoose.connection.on("connected", () => 
-            console.log("MongoDB connection ready")
-        );
 
-        mongoose.connection.on("error", (err) => 
-            console.error("MongoDB connection error:", err)
-        );
+    mongoose.connection.on("connected", () => 
+        console.log("MongoDB connection ready"))
 
-        await mongoose.connect(process.env.MONGODB_URI, { 
-            useNewUrlParser: true, 
-            useUnifiedTopology: true 
-        });
-        
-        console.log("Database connected successfully");
-    } catch (error) {
-        console.error("Failed to connect to MongoDB", error);
-        process.exit(1); // Exit with failure code
-    }
-};
+    await mongoose.connect('${process.env.MONGO_URI}/prescripto')
+}
 
-export default connectDB;
+export default connectDB
