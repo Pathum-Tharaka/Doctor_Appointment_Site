8e5418e6d6e24cd6918e5fda9d0ab0d1f5ddfdd9
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-10-31 00:43:10 +0530

    Admin side

---
+++ Admin/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { assets } from '../assets/assets_admin/assets'
+
+const Login = () => {
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Login
